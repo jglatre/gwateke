6d9d6f486d223c1dc4a2398afa3aad88822aed4f
jglatre <juanjo@simellegamemeo.org>
2011-01-30 03:27:27 +0100

    added new test cases

--- test/com/github/gwateke/client/AllTests.java
+++ test/com/github/gwateke/client/AllTests.java
@@ -1,12 +1,18 @@
 package com.github.gwateke.client;
 
-import javax.swing.DefaultListSelectionModelTest;
-
-import com.google.gwt.core.client.GWT;
-
 import gwtunit.client.GwtTestRunner;
 
+import javax.swing.DefaultListSelectionModelTest;
+
 import junit.framework.TestSuite;
+
+import com.github.gwateke.binding.adapter.SelectionInListTest;
+import com.github.gwateke.binding.validation.DefaultValidationResultsModelTest;
+import com.github.gwateke.binding.validation.DefaultValidationResultsTest;
+import com.github.gwateke.binding.value.support.MapKeyAdapterTest;
+import com.github.gwateke.binding.value.support.TypeConverterTest;
+import com.github.gwateke.binding.value.support.ValueHolderTest;
+import com.google.gwt.core.client.GWT;
 
 
 public class AllTests extends GwtTestRunner {
@@ -15,6 +21,12 @@ public class AllTests extends GwtTestRunner {
 		TestSuite suite = new TestSuite("gwateke tests");
 
 		suite.addTestSuite( GWT.<DefaultListSelectionModelTest>create(DefaultListSelectionModelTest.class) );
+		suite.addTestSuite( GWT.<SelectionInListTest>create(SelectionInListTest.class) );
+		suite.addTestSuite( GWT.<DefaultValidationResultsTest>create(DefaultValidationResultsTest.class) );
+		suite.addTestSuite( GWT.<DefaultValidationResultsModelTest>create(DefaultValidationResultsModelTest.class) );
+		suite.addTestSuite( GWT.<MapKeyAdapterTest>create(MapKeyAdapterTest.class) );
+		suite.addTestSuite( GWT.<TypeConverterTest>create( TypeConverterTest.class) );
+		suite.addTestSuite( GWT.<ValueHolderTest>create(ValueHolderTest.class) );
 		
 		return suite;
 	}
