66d6be76aae77aad1bd6b94f6c224496d2e239f9
jglatre <juanjo@simellegamemeo.org>
2011-02-13 22:24:21 +0100

    added some missing methods

--- src/com/github/gwateke/model/ManagementModel.java
+++ src/com/github/gwateke/model/ManagementModel.java
@@ -2,6 +2,11 @@ package com.github.gwateke.model;
 
 import com.github.gwateke.binding.value.ValueModel;
 import com.github.gwateke.command.ActionCommand;
+import com.github.gwateke.model.event.ActionHandler;
+import com.github.gwateke.model.event.ConfirmationHandler;
+import com.github.gwateke.model.event.ErrorHandler;
+import com.github.gwateke.model.event.MessageHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Command;
 
 
@@ -12,8 +17,16 @@ public interface ManagementModel {
 	}
 	
 	ValueModel<Status> getStatus();
+	void addNew();
+	void delete();
+	void refresh();
 	void startBrowse();
 	void cancelEdit();
 	boolean cancelEdit(Command postCancel);
 	ActionCommand getActionCommand(Action action);
+	
+	HandlerRegistration addErrorHandler(ErrorHandler handler);
+	HandlerRegistration addActionHandler(ActionHandler handler);
+	HandlerRegistration addMessageHandler(MessageHandler handler);
+	HandlerRegistration addConfirmationHandler(ConfirmationHandler handler);
 }
