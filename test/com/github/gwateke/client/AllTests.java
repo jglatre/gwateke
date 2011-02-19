a4f6c0e2be9edeb3a73d57247df8a4926966827f
jglatre <juanjo@simellegamemeo.org>
2011-02-19 01:00:17 +0100

    tests added

--- test/com/github/gwateke/client/AllTests.java
+++ test/com/github/gwateke/client/AllTests.java
@@ -1,9 +1,6 @@
 package com.github.gwateke.client;
 
 import gwtunit.client.GwtTestRunner;
-
-import javax.swing.DefaultListSelectionModelTest;
-
 import junit.framework.TestSuite;
 
 import com.github.gwateke.binding.adapter.SelectionInListTest;
@@ -12,6 +9,11 @@ import com.github.gwateke.binding.validation.DefaultValidationResultsTest;
 import com.github.gwateke.binding.value.support.MapKeyAdapterTest;
 import com.github.gwateke.binding.value.support.TypeConverterTest;
 import com.github.gwateke.binding.value.support.ValueHolderTest;
+import com.github.gwateke.command.support.ActionCommandTest;
+import com.github.gwateke.command.support.DefaultCommandRegistryTest;
+import com.github.gwateke.data.metadata.MetadataDictionaryTest;
+import com.github.gwateke.model.event.MessageEventTest;
+import com.github.gwateke.model.table.support.RowIdsSelectionHolderTest;
 import com.google.gwt.core.client.GWT;
 
 
@@ -20,13 +22,18 @@ public class AllTests extends GwtTestRunner {
 	protected TestSuite suite() {
 		TestSuite suite = new TestSuite("gwateke tests");
 
-		suite.addTestSuite( GWT.<DefaultListSelectionModelTest>create(DefaultListSelectionModelTest.class) );
 		suite.addTestSuite( GWT.<SelectionInListTest>create(SelectionInListTest.class) );
 		suite.addTestSuite( GWT.<DefaultValidationResultsTest>create(DefaultValidationResultsTest.class) );
 		suite.addTestSuite( GWT.<DefaultValidationResultsModelTest>create(DefaultValidationResultsModelTest.class) );
 		suite.addTestSuite( GWT.<MapKeyAdapterTest>create(MapKeyAdapterTest.class) );
-		suite.addTestSuite( GWT.<TypeConverterTest>create( TypeConverterTest.class) );
+		suite.addTestSuite( GWT.<TypeConverterTest>create(TypeConverterTest.class) );
 		suite.addTestSuite( GWT.<ValueHolderTest>create(ValueHolderTest.class) );
+		
+		suite.addTestSuite( GWT.<ActionCommandTest>create(ActionCommandTest.class) );
+		suite.addTestSuite( GWT.<DefaultCommandRegistryTest>create(DefaultCommandRegistryTest.class) );
+		suite.addTestSuite( GWT.<MetadataDictionaryTest>create(MetadataDictionaryTest.class) );
+		suite.addTestSuite( GWT.<MessageEventTest>create(MessageEventTest.class) );
+		suite.addTestSuite( GWT.<RowIdsSelectionHolderTest>create(RowIdsSelectionHolderTest.class) );
 		
 		return suite;
 	}
