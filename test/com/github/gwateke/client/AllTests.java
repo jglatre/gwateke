00452ea808df902d2315f766137362a8a188d371
jglatre <juanjo@simellegamemeo.org>
2011-02-20 14:40:01 +0100

    added EntityMetadataTest

--- test/com/github/gwateke/client/AllTests.java
+++ test/com/github/gwateke/client/AllTests.java
@@ -11,6 +11,7 @@ import com.github.gwateke.binding.value.support.TypeConverterTest;
 import com.github.gwateke.binding.value.support.ValueHolderTest;
 import com.github.gwateke.command.support.ActionCommandTest;
 import com.github.gwateke.command.support.DefaultCommandRegistryTest;
+import com.github.gwateke.data.metadata.EntityMetadataTest;
 import com.github.gwateke.data.metadata.MetadataDictionaryTest;
 import com.github.gwateke.model.event.MessageEventTest;
 import com.github.gwateke.model.table.support.RowIdsSelectionHolderTest;
@@ -31,6 +32,7 @@ public class AllTests extends GwtTestRunner {
 		
 		suite.addTestSuite( GWT.<ActionCommandTest>create(ActionCommandTest.class) );
 		suite.addTestSuite( GWT.<DefaultCommandRegistryTest>create(DefaultCommandRegistryTest.class) );
+		suite.addTestSuite( GWT.<EntityMetadataTest>create(EntityMetadataTest.class) );
 		suite.addTestSuite( GWT.<MetadataDictionaryTest>create(MetadataDictionaryTest.class) );
 		suite.addTestSuite( GWT.<MessageEventTest>create(MessageEventTest.class) );
 		suite.addTestSuite( GWT.<RowIdsSelectionHolderTest>create(RowIdsSelectionHolderTest.class) );
